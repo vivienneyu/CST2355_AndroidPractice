7bc74bd68af929cb06b633b86e516e25ebe1a711
yu000174@algonquinlive.com <yu000174@algonquinlive.com>
2020-06-25 00:41:38 -0400

    Add initPerson() method

--- app/src/main/java/com/example/mypractice/MainActivity.java
+++ app/src/main/java/com/example/mypractice/MainActivity.java
@@ -18,7 +18,7 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = "MainActivity";
-
+    private ArrayList<Person> peopleList;
 
 
     @Override
@@ -28,17 +28,7 @@ public class MainActivity extends AppCompatActivity {
         ListView mListView = findViewById(R.id.lv_theList);
        Log.d(TAG, "onCreate: started.");
 
-       //Create the Person objects
-       Person john = new Person("John", "12-20-2010", "Male");
-       Person peter = new Person("Peter", "12-12-2012", "Male");
-       Person jane = new Person("Jane", "13-12-2011", "Female");
-        Person joe = new Person("Joe", "14-12-2011", "Female");
-
-        ArrayList<Person> peopleList = new ArrayList<>();
-        peopleList.add(john);
-        peopleList.add(peter);
-        peopleList.add(jane);
-        peopleList.add(joe);
+       initPerson();
 
         PersonListAdapter adapter = new PersonListAdapter(this, R.layout.list_item_layout, peopleList);
         mListView.setAdapter(adapter);
@@ -56,6 +46,19 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+private void initPerson(){
+    //Create the Person objects
+    Person john = new Person("John", "12-20-2010", "Male");
+    Person peter = new Person("Peter", "12-12-2012", "Male");
+    Person jane = new Person("Jane", "13-12-2011", "Female");
+    Person joe = new Person("Joe", "14-12-2011", "Female");
 
+    peopleList = new ArrayList<>();
+    peopleList.add(john);
+    peopleList.add(peter);
+    peopleList.add(jane);
+    peopleList.add(joe);
+
+}
 
 }
